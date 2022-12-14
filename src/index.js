783c7db74396fad99750cf87a86d07dd00757b44
Ray Villalobos <ray@planetoftheweb.com>
2022-12-14 01:46:38 -0500

    Set fragments to false.

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ import Notes from 'reveal.js/plugin/notes/notes.esm'
 
 Reveal.initialize({
   plugins: [Markdown, Highlight, Notes],
-  fragments: true, // Globally turn off fragments
+  fragments: false, // Globally turn off fragments
   footer: false, //show footer menu
   margin: 0,
   minScale: 0,
